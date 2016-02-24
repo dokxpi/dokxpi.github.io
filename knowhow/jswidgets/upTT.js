287833a71b2d1fa3a1794d5afa8021fe5c2948c4
Arthur <dokxpi@gmail.com>
2016-02-24 11:36:07 +0200

    44

--- knowhow/jswidgets/upTT.js
+++ knowhow/jswidgets/upTT.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function() {
 	
-	// Виджет кнопки вверх (Test-Templates)
-	// Версия 1.0
+	// Виджет кнопки вверх 
+	// Версия 1.1
 	
 	$('body').append('<div class="button-up" style="display:none; opacity:0.7; width:80px; height:100%; position:fixed; left:0px; top:0px; cursor:pointer; text-align:center; line-height:100px; color:rgb(172, 188, 206); font-weight:100;"><font size="2">Наверх</font></div>');
 		
