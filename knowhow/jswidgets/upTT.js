7f6852435eefcddbf825db11e4e3dd3920467c3e
Arthur <dokxpi@gmail.com>
2016-02-25 00:13:03 +0200

    пппп

--- knowhow/jswidgets/upTT.js
+++ knowhow/jswidgets/upTT.js
@@ -1,9 +1,8 @@
 ﻿$(document).ready(function() {
 	
 	// Виджет кнопки вверх
-	// Версия 1.1
-	// http://dokxpi.github.io/knowhow/upTT/
-	// Второй способ: http://kostyakhmelev.ru/texnicheskaya-chast/knopka-vverh-dlya-sayta.html  
+	// Версия 1.1 https://dokxpi.github.io/knowhow/upTT/
+	// Второй способ http://kostyakhmelev.ru/texnicheskaya-chast/knopka-vverh-dlya-sayta.html  
 	
 	
 	$('body').append('<div class="button-up" style="display:none; opacity:0.7; width:85px; height:100%; position:fixed; left:0px; top:0px; cursor:pointer; text-align:center; line-height:100px; color:rgb(172, 188, 206); font-weight:100;"><font size="2">&#9650; Наверх</font></div>');
