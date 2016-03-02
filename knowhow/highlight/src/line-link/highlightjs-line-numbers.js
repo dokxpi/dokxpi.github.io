f2d86fed9fd368f24e9aaea4167b9a69bb2a0696
Arthur <dokxpi@gmail.com>
2016-03-02 18:19:38 +0200

    11

--- knowhow/highlight/src/line-link/highlightjs-line-numbers.js
+++ knowhow/highlight/src/line-link/highlightjs-line-numbers.js
@@ -8,30 +8,40 @@
 		w.hljs.lineNumbersBlock = lineNumbersBlock;
 	}
 
-	function initLineNumbersOnLoad () {
-		if (document.readyState === 'complete') {
-			documentReady();
-		} else {
-			w.addEventListener('DOMContentLoaded', documentReady);
-		}
-	}
+	var defaultOptions = {
+		withLinks: false
+	};
 
-	function documentReady () {
-		try {
-			var blocks = document.querySelectorAll('code.hljs');
+	function initLineNumbersOnLoad (options) {
+		options = options || defaultOptions;
 
-			for (var i in blocks) {
-				if (blocks.hasOwnProperty(i)) {
-					lineNumbersBlock(blocks[i]);
+		w.addEventListener('load', function () {
+			try {
+				var blocks = document.querySelectorAll('code.hljs');
+
+				for (var i in blocks) {
+					if (blocks.hasOwnProperty(i)) {
+						lineNumbersBlock(blocks[i], {
+							blockName: 'c' + i,
+							withLinks: options.withLinks
+						});
+					}
 				}
+			} catch (e) {
+				console.error('LineNumbers error: ', e);
 			}
-		} catch (e) {
-			console.error('LineNumbers error: ', e);
-		}
+		});
 	}
 
-	function lineNumbersBlock (element) {
+	function lineNumbersBlock (element, options) {
 		if (typeof element !== 'object') return;
+		if (!!options) {
+			options.withLinks = options.withLinks || false;
+			options.blockName = options.blockName || false;
+		} else {
+			options = defaultOptions;
+			options.blockName = '';
+		}
 
 		var parent = element.parentNode;
 		var lines = getCountLines(parent.textContent);
@@ -39,19 +49,21 @@
 		if (lines > 1) {
 			var l = '';
 			for (var i = 0; i < lines; i++) {
-				l += (i + 1) + '\n';
+				l +=  options.withLinks
+					? getLineWithLink(i + 1, options.blockName)
+					: (i + 1) + '\n';
 			}
 
 			var linesPanel = document.createElement('code');
 			linesPanel.className = 'hljs hljs-line-numbers';
 			linesPanel.style.float = 'left';
-			linesPanel.textContent = l;
+			linesPanel.innerHTML = l;
 
 			parent.insertBefore(linesPanel, element);
 		}
 	}
 
-	function getCountLines(text) {
+	function getCountLines (text) {
 		if (text.length === 0) return 0;
 
 		var regExp = /\r\n|\r|\n/g;
@@ -63,5 +75,10 @@
 		}
 
 		return lines;
+	}
+
+	function getLineWithLink (i, blockName) {
+		var id = blockName + '_l' + i;
+		return '<a href="#' + id + '" name="' + id + '">' + i + '</span>\n'
 	}
 }(window));
